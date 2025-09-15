316442b9c4a0494b9d150673d4c239c5ab9f90e0
easyhooon <mraz3068@gmail.com>
2025-09-15 13:50:09 +0900

    chore: network 모듈 ktor 의존성 추가

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.booksearch.android.library)
     alias(libs.plugins.booksearch.android.retrofit)
     alias(libs.plugins.booksearch.android.hilt)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -24,6 +25,12 @@ android {
 
 dependencies {
     implementation(libs.logger)
+
+    implementation(libs.ktor.client.okhttp)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.kotlinx.serialization.json)
+
+    implementation(libs.kotlinx.serialization.json)
 }
 
 fun getLocalProperty(propertyKey: String): String {
