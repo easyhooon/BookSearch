9555b178af9bf5c495d8cd461f9850b69e731af9
easyhooon <mraz3068@gmail.com>
2025-09-29 15:57:08 +0900

    feat: safeLaunchedEffect 도입

---
+++ core/common/src/main/kotlin/com/easyhooon/booksearch/core/common/compose/ComposeEffectErrorHandler.kt
@@ -0,0 +1,34 @@
+package com.easyhooon.booksearch.core.common.compose
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.staticCompositionLocalOf
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ensureActive
+
+interface ComposeEffectErrorHandler {
+    suspend fun emit(throwable: Throwable)
+}
+
+@Suppress("CompositionLocalAllowlist")
+val LocalComposeEffectErrorHandler = staticCompositionLocalOf<ComposeEffectErrorHandler> {
+    object : ComposeEffectErrorHandler {
+        override suspend fun emit(throwable: Throwable) {
+            throwable.printStackTrace()
+        }
+    }
+}
+
+@Composable
+fun SafeLaunchedEffect(key: Any?, block: suspend CoroutineScope.() -> Unit) {
+    val composeEffectErrorHandler = LocalComposeEffectErrorHandler.current
+    LaunchedEffect(key) {
+        try {
+            block()
+        } catch (e: Exception) {
+            ensureActive()
+            e.printStackTrace()
+            composeEffectErrorHandler.emit(e)
+        }
+    }
+}
